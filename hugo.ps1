cb6f996ff74dd22caa3169219a0bbdae823db02e
bboss204 <93838916+bboss204@users.noreply.github.com>
2025-05-22 15:16:10 +0200

    Update .gitignore, modify forme.csv, and enhance hugo.ps1 script

---
+++ hugo.ps1
@@ -0,0 +1,5 @@
+WmiObject -Class Win32_Product | Select-Object -Property Name, Version | Out-File -FilePath "C:\Users\bboss\Documents\GitHub\PowerShell\haha"
+
+for ($i = 0; $i -lt $array.Count; $i++) {
+    <# Action that will repeat until the condition is met #>
+}
