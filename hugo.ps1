5f008d1187ac05304bdee6d6f8b8350d2c0e72dc
bboss204 <93838916+bboss204@users.noreply.github.com>
2025-06-19 08:59:50 +0200

    hafgeiuef

--- hugo.ps1
+++ hugo.ps1
@@ -1,17 +1,13 @@
-# Récupérer les informations sur les logiciels installés
 $softwares = Get-WmiObject -Class Win32_Product | Select-Object -Property Name, Version, Vendor, InstallDate
 
-# Afficher les informations à l'écran
-Write-Host "Installed Software:"
+Write-Host "logiciel installé:"
 $softwares | Format-Table -AutoSize
 
-# Exporter les résultats dans un fichier CSV
 $outputFile = "SoftwareInventory.csv"
 $softwares | Export-Csv -Path $outputFile -NoTypeInformation
 
 Write-Host "Les informations sur les logiciels ont été exportées vers $outputFile"
 
-# Fonction pour ajouter manuellement un logiciel
 function Add-ManualSoftware {
     param (
         [string]$Name,
@@ -30,9 +26,7 @@ function Add-ManualSoftware {
     Write-Host "Logiciel ajouté : $Name"
 }
 
-# Exemple d'ajout manuel d'un logiciel
 Add-ManualSoftware -Name "MonLogiciel" -Version "1.0.0" -Vendor "MonEntreprise"
 
-# Réexporter les résultats mis à jour
 $softwares | Export-Csv -Path $outputFile -NoTypeInformation
 Write-Host "Les informations mises à jour sur les logiciels ont été exportées vers $outputFile"
