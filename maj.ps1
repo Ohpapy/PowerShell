11ce9b50487d55e317daaf7407253253279e1cc6
Ohpapy <twitch.mathis.boschian@gmail.com>
2025-06-19 08:22:21 +0200

    fin de maj

--- maj.ps1
+++ maj.ps1
@@ -1,52 +1,47 @@
-#RECUPERATION DES INFOS DU CSV
-
 $csvfile = Import-Csv -Path "./forme.csv"
 
-#LECTURE CVS ET TRAITEMENT DES MISE A JOUR
-
-#sortie de la commande winget search
-#Nom                     ID                             Version        Source
-#-----------------------------------------------------------------------------
-#Windows PC Health Check Microsoft.WindowsPCHealthCheck 3.7.2204.15001 winget
-
 foreach ($info in $csvfile) {
-        $name = $info.Name
-        $vendor = $info.Vendor
-        $version = $info.Version
-    if ($name -eq "none", $vendor -eq "none", $version -eq "none") {
-        Write-Output "Aucune entrée n'est renseignée, on ne peut pas la traiter"
-    } else {
-        if($vendor -like "*Microsoft*") {
+    $name = $info.Name
+    $vendor = $info.Vendor
+    $version = $info.Version
 
-        $update = winget search --name $name | Select-String $name
-        # Write-Output "Version locale : $version"
-        # Write-Output "Disponible sur winget : $update"
-        # Write-Output "Vendeur : $vendor"
-        # Write-Output "----------------------------------------"
-        
-        $ligne = $update -split '\s{2,}'
-        $versiondispo = $ligne[2]
+    if ($name -eq "none" -or $vendor -eq "none" -or $version -eq "none") {
+        Write-Output "Aucune entrée n'est renseignee, on ne peut pas la traiter"
+        continue
+    }
+
+    if ($vendor -like "*Microsoft*") {
+        $searchResult = winget search --name $name | Select-String $name
+
+        if (-not $searchResult) {
+            Write-Output "Aucune version trouvee sur winget pour $name"
+            continue
+        }
+
+        $ligne = ($searchResult -split '\s{2,}').Trim()
         $id = $ligne[1]
-        if ($versiondispo -eq ""){
-            Write-Output "Pas de version disponible sur winget"
-        } else {
-            if ($versiondispo -eq $version) {
-                Write-Output "La version est à jour"
-            } else {
-                Write-Output "La version de $name n'est pas a jour prenez la version $versiondispo"
-                Read-Host "Voulez-vous mettre à jour $name ? (O/N)"
-                $response = $input
-                if ($response -eq "O") {
-                    Write-Output "Mise à jour de $name"
-                    winget upgrade --id $id --version $versiondispo
-                } else {
-                    Write-Output "Mise à jour de $name annulée"
-                }
+        $versionDispo = $ligne[2]
+
+        if (-not $versionDispo -or $versionDispo.Trim() -eq "") {
+            Write-Output "Pas de version disponible sur winget pour $name"
+        }
+        elseif ($versionDispo -eq $version) {
+            Write-Output "La version de $name est à jour ($version)"
+        }
+        else {
+            Write-Output "La version de $name n'est pas a jour. Version installee : $version. Version disponible : $versionDispo"
+            $response = Read-Host "Voulez-vous mettre à jour $name ? (O/N)"
+
+            if ($response -eq "O") {
+                Write-Output "Mise à jour de $name..."
+                winget upgrade --id $id --version $versionDispo
+            }
+            else {
+                Write-Output "Mise à jour de $name abandon"
             }
         }
-        } else {
-            Write-Output "Le vendeur n'est pas Microsoft, on ne peut pas le traiter"
-
-        }
+    }
+    else {
+        Write-Output "Le vendeur n'est pas Microsoft, on ne peut pas le traiter"
     }
 }
